c9c5bb8cad65d6bf75273b8c9938863822741e4a
Chris Dzombak <chris@dzombak.com>
2022-01-19 16:16:39 -0500

    report additional statistics

--- main.go
+++ main.go
@@ -16,6 +16,40 @@ import (
 	"github.com/influxdata/influxdb-client-go/v2"
 )
 
+func readNut(ups, key string) (string, error) {
+	nutCmd := exec.Command("upsc", ups, key)
+	nutOut, err := nutCmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("failed to read %s: %s", key, err)
+	}
+	retv := strings.TrimSpace(string(nutOut))
+	return retv, nil
+}
+
+func readNutInt(ups, key string) (int, error) {
+	nutVal, err := readNut(ups, key)
+	if err != nil {
+		return 0, err
+	}
+	retv, err := strconv.Atoi(nutVal)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse %s '%s' to int: %s", key, nutVal, err)
+	}
+	return retv, nil
+}
+
+func readNutFloat(ups, key string) (float64, error) {
+	nutVal, err := readNut(ups, key)
+	if err != nil {
+		return 0, err
+	}
+	retv, err := strconv.ParseFloat(nutVal, 64)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse %s '%s' to float64: %s", key, nutVal, err)
+	}
+	return retv, nil
+}
+
 func main() {
 	var influxServer = flag.String("influx-server", "", "InfluxDB server, including protocol and port, eg. 'http://192.168.1.1:8086'. Required.")
 	var influxUser = flag.String("influx-username", "", "InfluxDB username.")
@@ -24,7 +58,8 @@ func main() {
 	var upsNameTag = flag.String("ups-nametag", "", "Value for the ups_name tag in InfluxDB. Required.")
 	var ups = flag.String("ups", "", "UPS to read status from, format 'upsname[@hostname[:port]]'. Required.")
 	var pollInterval = flag.Int("poll-interval", 30, "Polling interval, in seconds.")
-	var printUsage = flag.Bool("print-usage", false, "Log energy usage to standard error.")
+	var printUsage = flag.Bool("print-usage", false, "Log energy usage (in watts) to standard error.")
+	var influxTimeoutS = flag.Int("influx-timeout", 3, "Timeout for writing to InfluxDB, in seconds.")
 	flag.Parse()
 	if *influxServer == "" || *influxBucket == "" {
 		fmt.Println("-influx-bucket and -influx-server must be supplied.")
@@ -35,7 +70,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	const influxTimeout = 3 * time.Second
+	influxTimeout := time.Duration(*influxTimeoutS) * time.Second
 	authString := ""
 	if *influxUser != "" || *influxPass != "" {
 		authString = fmt.Sprintf("%s:%s", *influxUser, *influxPass)
@@ -55,32 +90,58 @@ func main() {
 	doUpdate := func() {
 		atTime := time.Now()
 
-		loadCmd := exec.Command("upsc", *ups, "ups.load")
-		loadOut, err := loadCmd.Output()
+		load, err := readNutInt(*ups, "ups.load")
 		if err != nil {
-			log.Printf("failed to read ups.load: %s", err)
+			log.Printf(err.Error())
 			return
 		}
-		load, err := strconv.Atoi(strings.TrimSpace(string(loadOut)))
+		nominalPower, err := readNutInt(*ups, "ups.realpower.nominal")
 		if err != nil {
-			log.Printf("failed to parse ups.load '%s' to int: %s", loadOut, err)
+			log.Printf(err.Error())
 			return
 		}
-
-		nominalPowerCmd := exec.Command("upsc", *ups, "ups.realpower.nominal")
-		nominalPowerOut, err := nominalPowerCmd.Output()
+		battCharge, err := readNutInt(*ups, "battery.charge")
 		if err != nil {
-			log.Printf("failed to read ups.realpower.nominal: %s", err)
+			log.Printf(err.Error())
 			return
 		}
-		nominalPower, err := strconv.Atoi(strings.TrimSpace(string(nominalPowerOut)))
+		battChargeLow, err := readNutInt(*ups, "battery.charge.low")
 		if err != nil {
-			log.Printf("failed to parse ups.realpower.nominal '%s' to int: %s", nominalPowerOut, err)
+			log.Printf(err.Error())
+			return
+		}
+		battRuntime, err := readNutInt(*ups, "battery.runtime")
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+		battV, err := readNutFloat(*ups, "battery.voltage")
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+		battVNominal, err := readNutInt(*ups, "battery.voltage.nominal")
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+		inputV, err := readNutFloat(*ups, "input.voltage")
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+		inputVNominal, err := readNutInt(*ups, "input.voltage.nominal")
+		if err != nil {
+			log.Printf(err.Error())
+			return
+		}
+		outputV, err := readNutFloat(*ups, "output.voltage")
+		if err != nil {
+			log.Printf(err.Error())
 			return
 		}
 
 		watts := math.Round(float64(nominalPower) * float64(load) / 100.0)
-
 		if *printUsage {
 			log.Printf("current approx. output for '%s': %.f watts", *ups, watts)
 		}
@@ -88,7 +149,18 @@ func main() {
 		point := influxdb2.NewPoint(
 			"ups_power_output",
 			map[string]string{"ups_name": *upsNameTag}, // tags
-			map[string]interface{}{"watts": watts},     // fields
+			map[string]interface{}{ // fields
+				"watts":                      watts,
+				"load_percent":               load,
+				"battery_charge_percent":     battCharge,
+				"battery_charge_low_percent": battChargeLow,
+				"battery_runtime_s":          battRuntime,
+				"battery_voltage":            battV,
+				"battery_voltage_nominal":    battVNominal,
+				"input_voltage":              inputV,
+				"input_voltage_nominal":      inputVNominal,
+				"output_voltage":             outputV,
+			},
 			atTime,
 		)
 		if err := retry.Do(
